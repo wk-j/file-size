20ec4b676c546e4a45c1551813a339483fea2632
wk <somnuk.wk@gmail.com>
2018-03-03 22:20:35 +0700

    Added unit options

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 
 ```
+rm ~/.dotnet/tools/wk-file-size
 cake -target=Pack
 dotnet install tool -g FileSize --source ./publish
 
 wk-file-size README.md
+wk-file-size -b README.md
+wk-file-size -k README.md
+wk-file-size -m README.md
+wk-file-size -g README.md
 ```
--- src/FileSize/Program.cs
+++ src/FileSize/Program.cs
@@ -1,25 +1,28 @@
 using System;
 using System.IO;
 
-namespace FileSize
-{
-    class Program
-    {
-        public static int Main(string[] args)
-        {
-            if (args.Length != 1)
-            {
-                Console.WriteLine($"Error - invalid arguments");
-                return -1;
-            }
+namespace FileSize {
 
-            var file = args[0];
+    enum Unit { Byte, KB, MB, GB }
+    class Program {
 
-            if (!File.Exists(file))
-            {
-                Console.WriteLine($"Error - {file} is not exist");
-                return -1;
+        private static (Unit, string) ParseCommands(string[] args) {
+
+            if (args.Length == 1) {
+                return (Unit.KB, args[0]);
+            } else if (args.Length == 2) {
+                var u = args[0];
+                var file = args[1];
+                if (u == "-b") return (Unit.Byte, file);
+                else if (u == "-m") return (Unit.MB, file);
+                else if (u == "-g") return (Unit.GB, file);
+                else return (Unit.KB, file);
+            } else {
+                throw new Exception("Error - Invalid argument");
             }
+        }
+
+        public static int Main(string[] args) {
 
             double toKb(double i) => i / 1024.0;
             double toMb(double i) => toKb(i) / 1024.0;
@@ -31,14 +34,22 @@ namespace FileSize
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.White;
 
-            var size = new FileInfo(file).Length + 0.0;
-            var format = string.Format(" {0} BYTE, {1:0.###} KB, {2:0.###} MB, {3:0.###} GB ",
-                size,
-                toKb(size),
-                toMb(size),
-                toGb(size));
+            var (unit, file) = ParseCommands(args);
+            if (!File.Exists(file)) {
+                throw new Exception($"Error - File \"{file}\" not exist");
+            }
 
-            Console.WriteLine(format);
+            var size = new FileInfo(file).Length;
+
+            if (unit == Unit.Byte) {
+                Console.WriteLine(String.Format("{0} {1}", size, unit));
+            } else if (unit == Unit.KB) {
+                Console.WriteLine(String.Format("{0:0.###} {1}", toKb(size), unit));
+            } else if (unit == Unit.MB) {
+                Console.WriteLine(String.Format("{0:0.###} {1}", toMb(size), unit));
+            } else if (unit == Unit.GB) {
+                Console.WriteLine(String.Format("{0:0.###} {1}", toGb(size), unit));
+            }
 
             Console.BackgroundColor = bg;
             Console.ForegroundColor = fg;
